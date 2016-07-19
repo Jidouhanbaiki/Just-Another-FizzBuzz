3fcc264709e1774f694f73e26c4b7b74c19f7e91
Andrew Shatov <andrii.shatov@gmail.com>
2016-07-19 13:34:53 +0300

    More fizzbuzz

--- code.py
+++ code.py
@@ -15,6 +15,22 @@ for rule in rules:
 print arr
 
 
+# take 1.a
+
+arr = [i for i in range(1, 101)]
+rules = ((15, "FizzBuzz"), (3, "Fizz"), (5, "Buzz"))
+
+
+def fizzbuzz(arr, div, word):
+	return map(lambda x: x if div.__rmod__(x) else word, arr)
+
+for rule in rules:
+	arr = fizzbuzz(arr, *rule)
+
+print arr
+
+
+
 # take 2
 
 arr = [i for i in range(1, 101)]
@@ -44,3 +60,20 @@ for i in range(1,101):
 	arr[-1] = arr[-1] or i
 
 print arr
+
+
+
+# take 4 - generator
+
+rules = ((3, "Fizz"), (5, "Buzz"))
+
+def fbrange(n, rules):
+	def fb(arr):
+		fb_str = "".join([elem for elem in arr if type(elem) == type("")])
+		return fb_str or arr[0]
+	i = 1
+	while i < n:
+		yield fb([i if i % r_num else r_str for r_num, r_str in rules])
+		i += 1
+
+print list(fbrange(101, rules))
